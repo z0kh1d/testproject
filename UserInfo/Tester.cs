cc230bb8e94292a052dc5e6946c21665d2cbf82b
Zohid Asatullayev <z0kh1d94@gmail.com>
2016-09-30 12:24:29 +0300

    New Class Added

---
+++ UserInfo/Tester.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace UserInfo
+{
+    public class Tester
+    {
+
+        public int Id { get; set; }
+        public string TesterName { get; set; }
+    }
+}
